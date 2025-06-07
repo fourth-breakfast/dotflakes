4af0b6c7a2490d802ec587aed3afd0634a7d0012
fourth-breakfast <fourth-breakfast@mailbox.org>
2025-06-07 05:47:19 +0200

    Added quiet and splash kernel parameters to kernel.nix

--- hosts/bakchos/modules/kernel.nix
+++ hosts/bakchos/modules/kernel.nix
@@ -1,12 +1,24 @@
-{pkgs, ...}:
+{ pkgs, ... }:
 
 {
-  # Select kernel
-  boot.kernelPackages = pkgs.linuxPackages_latest;
+
+  boot = {
+    kernelPackages = pkgs.linuxPackages_latest; # Select latest stable kernel
+
+    # Quiet boot with splash screen
+    kernelParams = [
+      "quiet"
+      "splash"
+    ];
+
+    # Enable Plymouth for graphical boot splash (optional)
+    plymouth = {
+      enable = true;
+      theme = "spinner"; # Simple spinner
+    };
+  };
 
   # Enable redistributable firmware
-  # hardware.enableRedistributableFirmware = true;
-
-  # Enable all firmware (including non-free)
-  hardware.enableAllFirmware = true;
+  hardware.enableRedistributableFirmware = true;
+  # hardware.enableAllFirmware = true; # Enable all firmware (including non-free)
 }
